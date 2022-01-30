6dca0525a271505b129fd3d1406db0fc3f0d3e1a
kemenchik <cemennnnnnnnn@gmail.com>
2022-01-30 23:59:28 +0300

    tabulation fix

--- platform_orchestrator/src/main/java/com/override/controllers/LessonController.java
+++ platform_orchestrator/src/main/java/com/override/controllers/LessonController.java
@@ -6,25 +6,24 @@ import dtos.TaskIdentifierDTO;
 import dtos.TestResultDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("lessons")
 public class LessonController {
 
     @Autowired
     private CodeExecutorFeign codeExecutorFeign;
 
-    @GetMapping("/lessons/{course}/{chapter}/{step}/{lesson}")
+    @GetMapping("{course}/{chapter}/{step}/{lesson}")
     public String lessonPage(@PathVariable String course, @PathVariable Integer chapter, @PathVariable Integer step,
                              @PathVariable Integer lesson) {
         return "lessons" + "/" + course + "/" + chapter + "/" + step + "/" + lesson;
     }
 
-    @PostMapping("/lessons")
-    public void getCodeTry(@RequestBody CodeTryDTO codeTryDTO) {
+    @PostMapping
+    @ResponseBody
+    public void getCodeTryResult(@RequestBody CodeTryDTO codeTryDTO) {
         codeExecutorFeign.execute(codeTryDTO);
     }
 }
