b9ed93c8ca732bfe7d4748d54fd73547c77aa8e3
kemenchik <cemennnnnnnnn@gmail.com>
2022-02-01 19:21:01 +0300

    fix pr

--- platform_orchestrator/src/main/java/com/override/controllers/LessonController.java
+++ platform_orchestrator/src/main/java/com/override/controllers/LessonController.java
@@ -1,10 +1,5 @@
 package com.override.controllers;
 
-import com.override.feigns.CodeExecutorFeign;
-import dtos.CodeTryDTO;
-import dtos.TaskIdentifierDTO;
-import dtos.TestResultDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,18 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("lessons")
 public class LessonController {
 
-    @Autowired
-    private CodeExecutorFeign codeExecutorFeign;
-
     @GetMapping("{course}/{chapter}/{step}/{lesson}")
     public String lessonPage(@PathVariable String course, @PathVariable Integer chapter, @PathVariable Integer step,
                              @PathVariable Integer lesson) {
         return "lessons" + "/" + course + "/" + chapter + "/" + step + "/" + lesson;
     }
 
-    @PostMapping
-    @ResponseBody
-    public void getCodeTryResult(@RequestBody CodeTryDTO codeTryDTO) {
-        codeExecutorFeign.execute(codeTryDTO);
-    }
 }
