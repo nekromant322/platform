feb0b3a6e1aa9924a7b3f9bdaed5a14148afef93
Nikita Schneider <free2fqn@gmail.com>
2022-02-01 14:37:44 +0300

    removed System.getEnv() from EmailMapper, now using @Value

--- platform_notification/src/main/java/com/override/util/EmailMapper.java
+++ platform_notification/src/main/java/com/override/util/EmailMapper.java
@@ -1,18 +1,21 @@
 package com.override.util;
 
 import dtos.MailDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 //TODO сейчас "From" достается из переменной-окружения. Но зачем-то же её можно регулировать, как настроить это?
 @Component
 public class EmailMapper {
+    @Value("${spring.mail.username}")
+    private String mailUserName;
+
     public SimpleMailMessage dtoToSimpleMail(MailDTO mailDTO) {
         SimpleMailMessage resultMail = new SimpleMailMessage();
-        resultMail.setFrom(System.getenv("MAIL_USER_NAME"));
+        resultMail.setFrom(mailUserName);
         resultMail.setTo(mailDTO.getTo().toArray(new String[mailDTO.getTo().size()]));
         if (mailDTO.getBcc() != null) {
             resultMail.setBcc(mailDTO.getBcc().toArray(new String[mailDTO.getBcc().size()]));
