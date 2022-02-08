6255b1c39c9cf0dffa1e1e190de7622b2dff1667
Nikita Schneider <free2fqn@gmail.com>
2022-02-08 12:23:47 +0300

    Added logger in NotificationController. If notification-service is unavailable log will write it, and getBalance will return "Service unavailable".

--- platform_orchestrator/src/main/java/com/override/controller/NotificationController.java
+++ platform_orchestrator/src/main/java/com/override/controller/NotificationController.java
@@ -1,12 +1,15 @@
 package com.override.controller;
 
 import com.override.feigns.NotificatorFeign;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/notification")
 public class NotificationController {
@@ -22,7 +25,14 @@ public class NotificationController {
 
     @GetMapping("/balance")
     public String getBalance() {
-        return notificatorFeign.getBalance();
+        String balance;
+        try {
+            balance = notificatorFeign.getBalance();
+        } catch (FeignException e) {
+            balance = "Service unavailable";
+            log.error("Caught FeignException: " + e.getMessage());
+        }
+        return balance;
     }
 
     @GetMapping("/url/replenish")
