5516fd85725db1e69587a1c71acdd6abbd1e8058
Nikita Schneider <free2fqn@gmail.com>
2022-02-26 21:49:44 +0300

    WIP added Sidebar-fragment at all lesson-files. Refactored lesson-files at columns. Removed LessonStructureDTO. Clean up.

--- platform_orchestrator/src/main/resources/static/js/lessonSidebar.js
+++ platform_orchestrator/src/main/resources/static/js/lessonSidebar.js
@@ -1,11 +1,12 @@
-window.onload = init("/core");
+/*TODO как в javascript сделать сюда инжект названия курса? Вообще идей нет, как считывать название выбранного  курса */
+window.onload = init("core");
 
 function init(courseName) {
     console.log("Hello world");
     let structure = {};
     $.ajax({
         method: "GET",
-        url: "/lessons/structureOf" + courseName, /*TODO добавить параметр для курса*/
+        url: "/lessons/structureOf/" + courseName,
         async: false,
         dataType: "json",
         success: function(data) {
@@ -17,8 +18,8 @@ function init(courseName) {
             console.log(error);
         }
     });
-    showStructure(structure);
-    // document.getElementById("sideBarBody").innerHTML = gatherText(structure);
+    document.getElementById("sideBarBody").innerHTML = " ";
+    document.getElementById("sideBarBody").innerHTML = gatherText(structure, courseName);
 }
 
 function showStructure(structureOfCourse) {
@@ -26,20 +27,35 @@ function showStructure(structureOfCourse) {
         console.log(chapterKey);
         for (let stepKey in structureOfCourse[chapterKey]) {
             console.log('\t' + stepKey);
+            for (let lessonKey in (structureOfCourse[chapterKey])[stepKey]) {
+                console.log('\t\t' + ((structureOfCourse[chapterKey])[stepKey])[lessonKey]);
+            }
         }
     }
 }
 
-//TODO разобраться c тем, как инжектить строки в строку ( ${something}})
-function gatherText(structureOfCourse) {
+function gatherText(structureOfCourse, courseName) {
     let text = "";
+    let nameOfLesson = "";
     for (let chapterKey in structureOfCourse) {
         text += ' <li className="active"> ';
-        text += ' <a href="#${chapterKey}Submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${chapterKey}</a> \n';
-        text += ' <ul class="collapse list-unstyled" id="${chapterKey}Submenu"> \n';
+        text += ` <a href="#${chapterKey}Submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${chapterKey}</a> \n`;
+        text += ` <ul class="collapse list-unstyled" id="${chapterKey}Submenu"> \n`;
         for (let stepKey in structureOfCourse[chapterKey]) {
-            text += ' <li> <a href="#">${stepKey}</a> </li> '
+            text += `<li> <a href="#${stepKey}Submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${stepKey}</a> </li>`;
+            text += ` <ul class="collapse list-unstyled" id="${stepKey}Submenu"> \n`;
+            for (let lessonKey in stepKey) {
+                text += '<li>';
+                if (((structureOfCourse[chapterKey])[stepKey])[lessonKey] !== undefined) {
+                    nameOfLesson = String(((structureOfCourse[chapterKey])[stepKey])[lessonKey]);
+                    nameOfLesson = nameOfLesson.substr(0, nameOfLesson.length - 5);
+                    text += `<a href="/lessons/${courseName}/${chapterKey}/${stepKey}/${nameOfLesson}">${nameOfLesson}</a> \n`; /*TODO почему-то /core/*/
+                }
+                text += '</li>';
+            }
+            text += '</ul>';
         }
+        text += '</ul>';
         text += ' </li> ';
     }
     return text;
