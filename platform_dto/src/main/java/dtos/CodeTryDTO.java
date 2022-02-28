cdbdeb5d09cc1340c014e0eb680de095c0d76bec
kemenchik <cemennnnnnnnn@gmail.com>
2022-02-28 23:50:52 +0300

    add valid by code size

--- platform_dto/src/main/java/dtos/CodeTryDTO.java
+++ platform_dto/src/main/java/dtos/CodeTryDTO.java
@@ -4,10 +4,14 @@ package dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Size;
+
+
 @Data
 @AllArgsConstructor
 public class CodeTryDTO {
 
     private TaskIdentifierDTO taskIdentifier;
+    @Size(max = 8096)
     private String studentsCode;
 }
--- platform_orchestrator/src/main/java/com/override/controller/rest/CodeTryController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/CodeTryController.java
@@ -3,10 +3,13 @@ package com.override.controller.rest;
 import com.override.feigns.CodeExecutorFeign;
 import dtos.CodeTryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("codeTry")
@@ -16,7 +19,10 @@ public class CodeTryController {
     private CodeExecutorFeign codeExecutorFeign;
 
     @PostMapping
-    public void getCodeTryResult(@RequestBody CodeTryDTO codeTryDTO) {
+    public void getCodeTryResult(@RequestBody @Valid CodeTryDTO codeTryDTO, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new IllegalArgumentException("Введенный код некорректен (не прошел по памяти)");
+        }
         codeExecutorFeign.execute(codeTryDTO);
     }
 }
