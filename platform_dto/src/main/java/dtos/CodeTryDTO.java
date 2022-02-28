32dc535eadf396e39f0ab42b1df11ddc139b5bb2
kemenchik <cemennnnnnnnn@gmail.com>
2022-03-01 01:47:33 +0300

    valid code by size

--- platform_dto/src/main/java/dtos/CodeTryDTO.java
+++ platform_dto/src/main/java/dtos/CodeTryDTO.java
@@ -1,13 +1,15 @@
 package dtos;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 public class CodeTryDTO {
 
     private TaskIdentifierDTO taskIdentifier;
+    @Size(max = 8096)
     private String studentsCode;
 }
--- platform_orchestrator/src/main/java/com/override/controller/rest/CodeTryController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/CodeTryController.java
@@ -3,10 +3,15 @@ package com.override.controller.rest;
 import com.override.feigns.CodeExecutorFeign;
 import dtos.CodeTryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("codeTry")
@@ -16,7 +21,11 @@ public class CodeTryController {
     private CodeExecutorFeign codeExecutorFeign;
 
     @PostMapping
-    public void getCodeTryResult(@RequestBody CodeTryDTO codeTryDTO) {
+    public ResponseEntity<String> getCodeTryResult(@RequestBody @Valid CodeTryDTO codeTryDTO, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("Введенный код некорректен (не прошел по длине)", HttpStatus.BAD_REQUEST);
+        }
         codeExecutorFeign.execute(codeTryDTO);
+        return new ResponseEntity<>("Все хорошо", HttpStatus.OK);
     }
 }
