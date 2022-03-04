3c109acf0f07bb43d30b0a005fe46806422cd9d1
kemenchik <cemennnnnnnnn@gmail.com>
2022-03-04 17:54:30 +0300

    delete unused

--- platform_orchestrator/src/main/java/com/override/configs/security/RestAuthenticationEntryPoint.java
+++ platform_orchestrator/src/main/java/com/override/configs/security/RestAuthenticationEntryPoint.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +12,7 @@ import java.io.IOException;
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
         httpServletResponse.sendRedirect("/login");
     }
 }
