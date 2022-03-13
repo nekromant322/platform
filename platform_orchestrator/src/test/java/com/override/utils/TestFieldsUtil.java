8ebd05c6308bc581a802556cb07b02ccaa4baa02
kemenchik <cemennnnnnnnn@gmail.com>
2022-03-13 11:49:59 +0300

    few tests + micro refactoring jwtprowider

--- platform_orchestrator/src/test/java/com/override/utils/TestFieldsUtil.java
+++ platform_orchestrator/src/test/java/com/override/utils/TestFieldsUtil.java
@@ -12,16 +12,17 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 
 public class TestFieldsUtil {
-    public static String generateTestCode(){
+
+    public static String generateTestCode() {
         return "public class MyClass {public static void main(String[] args){ System.out.println(\"This is my Code\");}}";
     }
 
-    public static PlatformUser generateTestUser(){
+    public static PlatformUser generateTestUser() {
         return new PlatformUser(null, "Andrey", "a", "a",
                 Collections.singletonList(new Authority(null, "admin")));
     }
 
-    public static TaskIdentifierDTO generateTestTaskIdentifierDTO(){
+    public static TaskIdentifierDTO generateTestTaskIdentifierDTO() {
         return TaskIdentifierDTO.builder()
                 .chapter(1)
                 .step(1)
@@ -29,20 +30,20 @@ public class TestFieldsUtil {
                 .build();
     }
 
-    public static CodeTry generateTestCodeTry(){
+    public static CodeTry generateTestCodeTry() {
         return new CodeTry(null, generateTestCode(), 1, 1, 1,
                 CodeExecutionStatus.OK, LocalDateTime.of(2022, 1, 1, 12, 0),
                 generateTestUser());
     }
 
-    public static TestResultDTO generateTestTestResultDTO(){
+    public static TestResultDTO generateTestTestResultDTO() {
         return TestResultDTO.builder()
                 .codeExecutionStatus(CodeExecutionStatus.OK)
                 .output("blabla")
                 .build();
     }
 
-    public static CodeTryDTO generateTestCodeTryDTO(){
+    public static CodeTryDTO generateTestCodeTryDTO() {
         return new CodeTryDTO(generateTestTaskIdentifierDTO(), generateTestCode());
     }
 }
