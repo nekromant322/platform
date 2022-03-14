049988a6f2eda95ecc62db2ab0bdc56c22cd745f
kemenchik <cemennnnnnnnn@gmail.com>
2022-03-14 13:23:54 +0300

    import fix

--- platform_orchestrator/src/main/java/com/override/service/PlatformUserService.java
+++ platform_orchestrator/src/main/java/com/override/service/PlatformUserService.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
