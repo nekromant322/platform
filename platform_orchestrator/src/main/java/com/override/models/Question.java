54b2ce33ffc2ecf57a2347c5af9f761447627709
ushackov <ushackov@gmail,com>
2022-03-18 17:26:03 +0300

    Question model created

--- platform_orchestrator/src/main/java/com/override/models/Question.java
+++ platform_orchestrator/src/main/java/com/override/models/Question.java
@@ -1,4 +1,27 @@
 package com.override.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column
+    String question;
+
+    @Column
+    int chapter;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private PlatformUser user;
 }
