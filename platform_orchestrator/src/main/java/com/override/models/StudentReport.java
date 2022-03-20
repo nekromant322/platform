a5174a6a86992f40fa3ced4ac9bee1fa0961766b
Nikita Schneider <free2fqn@gmail.com>
2022-03-21 01:22:31 +0300

    Removed h2, added PostgreSQL support. Credentials for Postgres added in orchestrator's application.yml. StudentReport now entity

--- platform_orchestrator/src/main/java/com/override/models/StudentReport.java
+++ platform_orchestrator/src/main/java/com/override/models/StudentReport.java
@@ -4,8 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,7 +19,17 @@ public class StudentReport {
 
     //TODO: когда будет секурити добавить автора
     //private Student student;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
     private LocalDate date;
     private String text;
     private Double hours;
+
+    public StudentReport(LocalDate date, String text, Double hours) {
+        this.date = date;
+        this.text = text;
+        this.hours = hours;
+    }
 }
