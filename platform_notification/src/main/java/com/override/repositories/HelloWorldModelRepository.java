9080bf91e3aa5e1c9f9358ac63d1889ebd5fbcac
Nikita Schneider <free2fqn@gmail.com>
2022-03-22 19:13:47 +0300

    delete initservice in Notification. Clean up

--- platform_notification/src/main/java/com/override/repositories/HelloWorldModelRepository.java
+++
@@ -1,6 +0,0 @@
-package com.override.repositories;
-
-import com.override.models.HelloWorldModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HelloWorldModelRepository extends CrudRepository<HelloWorldModel, Long> { }
--- platform_notification/src/main/java/com/override/service/HelloWorldService.java
+++
@@ -1,7 +0,0 @@
-package com.override.service;
-
-import com.override.models.HelloWorldModel;
-
-public interface HelloWorldService {
-    void save(HelloWorldModel helloWorldModel);
-}
