6b0bd430086ab7d7caf40377584690f0e0f86e2a
MaxWell <xaxaxa25420@gmail.com>
2022-03-25 19:56:25 +0300

    Added codeTry randomize

--- platform_orchestrator/src/main/java/com/override/util/InitializationService.java
+++ platform_orchestrator/src/main/java/com/override/util/InitializationService.java
@@ -105,24 +105,46 @@ public class InitializationService {
 
     private void codeTryInit() {
         List<PlatformUser> students = userService.getAllStudents();
-        students.forEach(student ->
-                codeTryService.saveCodeTry(
-                        CodeTryDTO
+        students.forEach(this::addCodeRetry);
+    }
+
+    private void addCodeRetry(PlatformUser student) {
+        CodeExecutionStatus status;
+        for (int i = 0; i < 10; i++) {
+            status = CodeExecutionStatus
+                    .values()[new Random().nextInt(CodeExecutionStatus.values().length)];
+            while (status != CodeExecutionStatus.OK) {
+                saveCodeTry(student, i, status);
+                status = CodeExecutionStatus
+                        .values()[new Random().nextInt(CodeExecutionStatus.values().length)];
+            }
+            saveCodeTry(student, i, CodeExecutionStatus.OK);
+
+        }
+    }
+
+    private void saveCodeTry(PlatformUser student, int task, CodeExecutionStatus status) {
+        String helloWorld = "class HelloWorld {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        System.out.println(\"Hello World!\");\n" +
+                "    }\n" +
+                "}";
+        codeTryService.saveCodeTry(
+                CodeTryDTO
+                        .builder()
+                        .taskIdentifier(TaskIdentifierDTO
                                 .builder()
-                                .taskIdentifier(TaskIdentifierDTO
-                                        .builder()
-                                        .chapter(faker.number().numberBetween(0, 10))
-                                        .lesson(faker.number().numberBetween(0, 10))
-                                        .step(faker.number().numberBetween(0, 10))
-                                        .build())
-                                .studentsCode(faker.funnyName().name())
-                                .build(),
-                        TestResultDTO
-                                .builder()
-                                .codeExecutionStatus(CodeExecutionStatus
-                                        .values()[new Random().nextInt(CodeExecutionStatus.values().length)])
-                                .output(faker.funnyName().name()).build(),
-                        student.getLogin()));
+                                .chapter(task)
+                                .lesson(task)
+                                .step(task)
+                                .build())
+                        .studentsCode(helloWorld)
+                        .build(),
+                TestResultDTO
+                        .builder()
+                        .codeExecutionStatus(status)
+                        .output(faker.funnyName().name()).build(),
+                student.getLogin());
     }
 
     private void joinRequestsInit() {
