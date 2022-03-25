fbef4b6e0d079e349672216f74ff822b70a500a3
MaxWell <xaxaxa25420@gmail.com>
2022-03-25 17:15:32 +0300

    Completed task requirements. Edited InitializationService. Added @AllArgsConstructor for RegisterUserRequestDTO, TaskIdentifierDTO, TestResultDTO.

--- platform_dto/src/main/java/dtos/TestResultDTO.java
+++ platform_dto/src/main/java/dtos/TestResultDTO.java
@@ -1,11 +1,14 @@
 package dtos;
 
 import enums.CodeExecutionStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class TestResultDTO {
     private CodeExecutionStatus codeExecutionStatus;
     private String output;
--- platform_orchestrator/src/main/java/com/override/util/InitializationService.java
+++ platform_orchestrator/src/main/java/com/override/util/InitializationService.java
@@ -1,20 +1,31 @@
 package com.override.util;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import com.override.models.Authority;
 import com.override.models.PlatformUser;
 import com.override.models.enums.Role;
 import com.override.service.AuthorityService;
+import com.override.service.CodeTryService;
+import com.override.service.JoinRequestService;
 import com.override.service.PlatformUserService;
+import dtos.CodeTryDTO;
+import dtos.RegisterUserRequestDTO;
+import dtos.TaskIdentifierDTO;
+import dtos.TestResultDTO;
+import enums.CodeExecutionStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Component
-public class InitializationService implements CommandLineRunner {
+public class InitializationService {
 
     @Value("${jwt.primeAdminLogin}")
     private String adminLogin;
@@ -22,7 +33,13 @@ public class InitializationService implements CommandLineRunner {
     @Value("${jwt.primeAdminLogin}")
     private String adminPassword;
 
+    @Value("@admin")
+    private String adminTelegramChatId;
+
+    Faker faker = new Faker();
+
     private static final int COUNT_USER_FOR_INIT = 5;
+    private static final int COUNT_REQUESTS_FOR_INIT = 2;
 
     @Autowired
     private AuthorityService authorityService;
@@ -30,11 +47,19 @@ public class InitializationService implements CommandLineRunner {
     @Autowired
     private PlatformUserService userService;
 
-    @Override
-    public void run(String... args) {
+    @Autowired
+    private CodeTryService codeTryService;
+
+    @Autowired
+    private JoinRequestService joinRequestService;
+
+    @PostConstruct
+    private void init() {
         AuthorityInit();
-        UserInit();
         AdminInit();
+        UserInit();
+        codeTryInit();
+        joinRequestsInit();
     }
 
     private void AuthorityInit() {
@@ -45,19 +70,22 @@ public class InitializationService implements CommandLineRunner {
 
     private void UserInit() {
         for (int i = 0; i < COUNT_USER_FOR_INIT; i++) {
-            saveUser(i + "login", String.valueOf(i), Role.USER);
+            saveUser(faker.name().username(),
+                    faker.bothify("########"),
+                    String.valueOf(faker.number().numberBetween(1000, 10000)),
+                    Role.USER);
         }
     }
 
     private void AdminInit() {
-        saveUser(adminLogin, adminPassword, Role.USER, Role.ADMIN);
+        saveUser(adminLogin, adminPassword, adminTelegramChatId, Role.USER, Role.ADMIN);
     }
 
-    private PlatformUser saveUser(String login, String password, Role... userRoles) {
+    private void saveUser(String login, String password, String telegramChatId, Role... userRoles) {
         List<Authority> roles = getAuthorityListFromRoles(userRoles);
 
-        PlatformUser account = new PlatformUser(null, login, password, null, roles);
-        return userService.save(account);
+        PlatformUser account = new PlatformUser(null, login, password, telegramChatId, roles);
+        userService.save(account);
     }
 
     private List<Authority> getAuthorityListFromRoles(Role... roles) {
@@ -67,4 +95,29 @@ public class InitializationService implements CommandLineRunner {
         }
         return authorityList;
     }
+
+    private void codeTryInit() {
+        List<PlatformUser> students = userService.getAllStudents();
+        students.forEach(student ->
+                codeTryService.saveCodeTry(
+                        new CodeTryDTO(
+                                new TaskIdentifierDTO(
+                                        faker.number().numberBetween(0, 10),
+                                        faker.number().numberBetween(0, 10),
+                                        faker.number().numberBetween(0, 10)),
+                                faker.funnyName().name()),
+                        new TestResultDTO(
+                                CodeExecutionStatus.values()[new Random().nextInt(CodeExecutionStatus.values().length)],
+                                faker.funnyName().name()),
+                        student.getLogin()));
+    }
+
+    private void joinRequestsInit() {
+        for (int i = 0; i < COUNT_REQUESTS_FOR_INIT; i++) {
+            joinRequestService.saveRequest(new RegisterUserRequestDTO(faker.name().username(),
+                    String.valueOf(faker.number().numberBetween(1000,10000))));
+        }
+    }
+
+
 }
