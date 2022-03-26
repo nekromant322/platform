d2291f7046695fe1a16f080d2257c951106d2dbe
MaxWell <xaxaxa25420@gmail.com>
2022-03-26 14:09:40 +0300

    Faker -> bean

---
+++ platform_orchestrator/src/main/java/com/override/service/FakerService.java
@@ -0,0 +1,8 @@
+package com.override.service;
+
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FakerService extends Faker {
+}
--- platform_orchestrator/src/main/java/com/override/util/InitializationService.java
+++ platform_orchestrator/src/main/java/com/override/util/InitializationService.java
@@ -52,7 +52,8 @@ public class InitializationService {
     @Autowired
     private LessonStructureService lessonStructureService;
 
-    private final Faker faker = new Faker();
+    @Autowired
+    private Faker faker;
 
     @PostConstruct
     private void init() {
