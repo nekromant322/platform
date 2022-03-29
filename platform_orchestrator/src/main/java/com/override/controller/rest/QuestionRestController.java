89a99f75ac8e7262b00ce2cb5b73e57fb7278f93
MaxWell <xaxaxa25420@gmail.com>
2022-03-30 01:20:26 +0300

    All functional realized

--- platform_orchestrator/src/main/java/com/override/controller/rest/QuestionRestController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/QuestionRestController.java
@@ -1,11 +1,14 @@
 package com.override.controller.rest;
 
+import com.override.models.PlatformUser;
 import com.override.models.Question;
 import com.override.service.LessonStructureService;
+import com.override.service.PlatformUserService;
 import com.override.service.QuestionService;
 import dtos.QuestionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,16 +18,29 @@ import java.util.List;
 public class QuestionRestController {
     @Autowired
     private QuestionService questionService;
+
     @Autowired
     LessonStructureService lessonStructureService;
 
-    @GetMapping("/users")
-    public List<Question> findAll(@RequestBody QuestionDTO questionDTO) {
-            return questionService.findAllByUserAndChapter(questionDTO.getLogin(), questionDTO.getChapter());
+    @Autowired
+    PlatformUserService platformUserService;
+
+    @GetMapping("/users/{login}/{chapter}")
+    public List<Question> findAll(@PathVariable String login,
+                                  @PathVariable String chapter) {
+        return questionService.findAllByUserAndChapter(login, chapter);
+    }
+
+    @GetMapping("/get/current")
+    public PlatformUser getUserInfo() {
+        return platformUserService.findPlatformUserByLogin(SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName());
     }
 
     @GetMapping("/chapters")
-    public List<String> findAllChapters(){
+    public List<String> findAllChapters() {
         return lessonStructureService.getChapterNamesList();
     }
 
@@ -36,13 +52,13 @@ public class QuestionRestController {
 
     @Secured("ROLE_ADMIN")
     @PatchMapping
-    public void patch(@RequestBody QuestionDTO questionDTO){
+    public void patch(@RequestBody QuestionDTO questionDTO) {
         questionService.patch(questionDTO);
     }
 
     @Secured("ROLE_ADMIN")
     @DeleteMapping
-    public void delete(@RequestParam long id){
-            questionService.delete(id);
+    public void delete(@RequestParam long id) {
+        questionService.delete(id);
     }
 }
--- platform_orchestrator/src/main/resources/static/js/questions.js
+++ platform_orchestrator/src/main/resources/static/js/questions.js
@@ -1,15 +1,288 @@
-window.onload = checkUser()
+window.onload = function () {
+    getCurrentUser();
+};
 
-function checkUser() {
+function getCurrentUser() {
     $.ajax({
-        url: 'chapters',
+        url: 'questions/get/current',
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (currentUser) {
+            try {
+                if (currentUser.authorities[0].authority === 'ROLE_ADMIN' ||
+                    currentUser.authorities[1].authority === 'ROLE_ADMIN') {
+                    listAllUsers();
+                }
+            } catch (e) {
+                $("#allUserList").hide()
+                $("#tabContent").hide()
+                getStudentQuestions(currentUser.login)
+            }
+        }
+    })
+}
+
+function getStudentQuestions(studentLogin) {
+    let tab = $('#forSingleUser')
+    var tableId;
+    var chapterName
+    $.ajax({
+        url: 'questions/chapters',
         type: 'GET',
         contentType: 'application/json',
         success: function (chapters) {
-            $("#lessonStructure").empty()
+            tab.empty()
             $.each(chapters, function (i, chapter) {
-                $("#lessonStructure").append('<td>' + chapter + '</td>')
+                chapterName = chapter.replaceAll(' ', '')
+                tableId = studentLogin + chapterName + 'table'
+                tab.append('<br>' +
+                    '<div class="card" style="background-color: #f8f9fa">' +
+                    '<div class="card-body">' +
+                    '<div class="table-responsive">' +
+                    '<table class="table table-hover col">' +
+                    '<thead class="table-light" >' +
+                    '<tr role="row">' +
+                    '<td><h5>' + chapter + '</h5></td>' +
+                    '</tr>' +
+                    '</thead>' +
+                    '<tbody class="table-light" id="' + tableId + '">' +
+                    '<tr><td><div id="accordion' + studentLogin + i + '">' +
+                    '<div class="card">' +
+                    '<div class="card-header" id="heading' + studentLogin + i + '">' +
+                    '<h5 class="mb-0">' +
+                    '<button class="btn btn-link" data-toggle="collapse" data-target="#collapse' + studentLogin + i + '" ' +
+                    'aria-expanded="true" aria-controls="collapse' + studentLogin + i + '"> Отвеченные вопросы' +
+                    '</button>' +
+                    '</h5>' +
+                    '</div>' +
+                    '<div id="collapse' + studentLogin + i + '" class="collapse" ' +
+                    'aria-labelledby="heading' + studentLogin + i + '" data-parent="#accordion' + studentLogin + i + '">' +
+                    '<div class="card-body">' +
+                    '<div class="table-responsive">' +
+                    '<table class="table table-hover col">' +
+                    '<tbody class="table" id="' + tableId + 'Answered">' +
+                    '</tbody></table>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</td></tr></tbody>' +
+                    '</table>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div><br>')
+
+                getQuestionsForStudent(studentLogin, chapter, tableId)
             })
+        }
+
+    })
+}
+
+function getQuestionsForStudent(studentLogin, chapter, table) {
+    let tableId = $('#' + table)
+    let tableAnswered = $('#' + table + 'Answered')
+    $.ajax({
+        url: 'questions/users/' + studentLogin + '/' + chapter,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (questions) {
+            $.each(questions, function (i, question) {
+                if (question.answered === true) {
+                    tableAnswered.append('<tr><td><s>' + question.question + '</s></td></tr>')
+                } else {
+                    tableId.append('<tr><td>' + question.question + '</td></tr>')
+                }
+            })
+        }
+    })
+}
+
+
+function listAllUsers() {
+    var navLinkId;
+    var vPillId;
+    $.ajax({
+        url: '/admin/getAllStudents',
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (users) {
+            $("#allUserList").empty()
+            $("#tabContent").empty()
+
+            $.each(users, function (i, user) {
+                navLinkId = i + 'navLink'
+                vPillId = 'v-pills-' + i
+                $("#allUserList").append('<a class="nav-link" id="' + navLinkId + '" data-toggle="pill" role="tab" ' +
+                    'aria-controls="v-pills-' + i + '"  aria-selected="false">' + user.login + '</a>')
+                $("#tabContent").append('<div class="tab-pane" id="' + vPillId + '" role="tabpanel" ' +
+                    '></div>')
+
+                $("#" + navLinkId).attr("href", "#" + vPillId)
+                checkLessonStructure(user.login, vPillId)
+
+            })
+            $("#v-pills-0").addClass('active')
+            $("#0navLink").attr("aria-selected", true)
+            $("#0navLink").addClass('active')
+        }
+    })
+}
+
+function checkLessonStructure(studentLogin, tabPane) {
+    let tab = $('#' + tabPane)
+    var tableId;
+    var chapterName
+    $.ajax({
+        url: 'questions/chapters',
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (chapters) {
+            tab.empty()
+            $.each(chapters, function (i, chapter) {
+                chapterName = chapter.replaceAll(' ', '')
+                tableId = studentLogin + chapterName + 'table'
+                tab.append('<br>' +
+                    '<div class="card" style="background-color: #f8f9fa">' +
+                    '<div class="card-body" >' +
+                    '<div class="table-responsive">' +
+                    '<table class="table table-hover col">' +
+                    '<thead class="table-light" >' +
+                    '<tr role="row">' +
+                    '<td><h5>' + chapter + '</h5></td>' +
+                    '</tr>' +
+                    '</thead>' +
+                    '<tbody class="table-light" id="' + tableId + '">' +
+                    '<tr><td><div id="accordion' + studentLogin + i + '">' +
+                    '<div class="card">' +
+                    '<div class="card-header" id="heading' + studentLogin + i + '">' +
+                    '<h5 class="mb-0">' +
+                    '<button class="btn btn-link" data-toggle="collapse" data-target="#collapse' + studentLogin + i + '" ' +
+                    'aria-expanded="true" aria-controls="collapse' + studentLogin + i + '"> Отвеченные вопросы' +
+                    '</button>' +
+                    '</h5>' +
+                    '</div>' +
+                    '<div id="collapse' + studentLogin + i + '" class="collapse" ' +
+                    'aria-labelledby="heading' + studentLogin + i + '" data-parent="#accordion' + studentLogin + i + '">' +
+                    '<div class="card-body">' +
+                    '<div class="table-responsive">' +
+                    '<table class="table table-hover col">' +
+                    '<tbody class="table" id="' + tableId + 'Answered">' +
+                    '</tbody></table>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</td></tr></tbody>' +
+                    '</table>' +
+                    '<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="collapse" ' +
+                    'data-target="#collapseButton' + studentLogin + i + '" aria-expanded="false" aria-controls="collapseButton' + studentLogin + i + '" ' +
+                    'id="button' + studentLogin + i + '">Добавить вопрос</button>' +
+                    '<div class="collapse" id="collapseButton' + studentLogin + i + '">' +
+                    '<div class="card card-body">' +
+                    '<form id="form' + studentLogin + i + '" >' +
+                    '<div class="form-group">' +
+                    '<input class="form-control" id="newQuestion' + studentLogin + i + '" placeholder="Вопрос" required><br>' +
+                    '<button type="submit" id="addButton' + studentLogin + i + '" class="btn btn-success">Добавить</button>' +
+                    '</div>' +
+                    '</form>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div></div></div><br>')
+
+                getQuestionsByChapter(studentLogin, chapter, tableId, tabPane)
+
+                $('#form' + studentLogin + i).submit(function () {
+                    saveQuestion(null, $('#newQuestion' + studentLogin + i).val(), chapter, false, studentLogin)
+                    checkLessonStructure(studentLogin, tabPane)
+                })
+            })
+        }
+    })
+}
+
+function getQuestionsByChapter(studentLogin, chapter, table, tabPane) {
+    let tableId = $('#' + table)
+    let tableAnswered = $('#' + table + 'Answered')
+    $.ajax({
+        url: 'questions/users/' + studentLogin + '/' + chapter,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (questions) {
+            $.each(questions, function (i, question) {
+                if (question.answered === true) {
+                    tableAnswered.append('<tr><td><div class="container-fluid"><div class="row">' +
+                        '<div class="col-11"><div class="form-check form-check-inline">' +
+                        '<input class="form-check-input" type="checkbox" ' +
+                        'id="checkbox' + studentLogin + i + chapter.id + '" checked/>' +
+                        '</div><s>'
+                        + question.question +
+                        '</s></div>' +
+                        '<div class="col"><button type="button" id="del' + studentLogin + i + table + '"' +
+                        ' class="btn btn-danger btn-sm">Удалить</button>' +
+                        '</div></div></div></td></tr>')
+
+                    $('#checkbox' + studentLogin + i + chapter.id).change(function () {
+                        if (this.checked !== true) {
+                            saveQuestion(question.id, question.question, question.chapter, false, studentLogin, tabPane)
+                            checkLessonStructure(studentLogin, tabPane)
+                        }
+                    })
+                } else {
+                    tableId.append('<tr><td><div class="container-fluid"><div class="row">' +
+                        '<div class="col-11"><div class="form-check form-check-inline">' +
+                        '<input class="form-check-input" type="checkbox" id="checkbox' + studentLogin + i + chapter.id + '">' +
+                        '</div>'
+                        + question.question +
+                        '</div>' +
+                        '<div class="col"><button type="button" id="del' + studentLogin + i + table + '" ' +
+                        'class="btn btn-danger btn-sm">Удалить</button>' +
+                        '</div></div></div></td></tr>')
+
+                    $('#checkbox' + studentLogin + i + chapter.id).change(function () {
+                        if (this.checked) {
+                            saveQuestion(question.id, question.question, question.chapter, true, studentLogin, tabPane)
+                            checkLessonStructure(studentLogin, tabPane)
+                        }
+                    })
+                }
+                $('#del' + studentLogin + i + table).click(function () {
+                    deleteQuestion(question.id)
+                    checkLessonStructure(studentLogin, tabPane)
+                })
+            })
+        }
+    })
+}
+
+function deleteQuestion(id) {
+    $.ajax({
+        url: 'questions?id=' + id,
+        method: 'DELETE',
+        success: function () {
+            console.log('deleted')
+        }
+    })
+}
+
+function saveQuestion(id, question, chapter, answered, login) {
+    $.ajax({
+        url: 'questions',
+        dataType: 'json',
+        method: 'PATCH',
+        cache: false,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            id: id,
+            question: question,
+            chapter: chapter,
+            answered: answered,
+            login: login,
+        }),
+        success: function () {
+            console.log('saved')
         }
     })
 }
