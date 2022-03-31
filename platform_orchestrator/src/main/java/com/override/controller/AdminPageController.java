c8ddac94244f5854d20edef1895f005d2a3051d6
MaxWell <xaxaxa25420@gmail.com>
2022-04-01 02:35:13 +0300

    AdminPageController edit

--- platform_orchestrator/src/main/java/com/override/controller/AdminPageController.java
+++ platform_orchestrator/src/main/java/com/override/controller/AdminPageController.java
@@ -1,23 +1,26 @@
 package com.override.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminPageController {
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/balancePage")
     public String balancePage() {
         return "balanceCheck";
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/allStudents")
     public String allStudentsPage() {
         return "allStudents";
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/joinRequests")
     public String joinRequestsPage() {
         return "joinRequests";
