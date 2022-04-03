fee63944cb73b332186db4f7b915f4e3d6100b03
MaxWell <xaxaxa25420@gmail.com>
2022-04-03 04:37:25 +0300

    PersonalData data structure filling

--- platform_orchestrator/src/main/java/com/override/models/PersonalData.java
+++ platform_orchestrator/src/main/java/com/override/models/PersonalData.java
@@ -1,16 +1,59 @@
 package com.override.models;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class PersonalData {
 
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "actNumber")
+    private Long actNumber;
+
+    @Column(name = "contractNumber")
+    private String contractNumber;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "fullName")
+    private String fullName;
+
+    @Column(name = "passportSeries")
+    private Long passportSeries;
+
+    @Column(name = "passportNumber")
+    private Long passportNumber;
+
+    @Column(name = "passportIssued")
+    private String passportIssued;
+
+    @Column(name = "issueDate")
+    private Date issueDate;
+
+    @Column(name = "birthDate")
+    private Date birthDate;
+
+    @Column(name = "registration")
+    private String registration;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phoneNumber")
+    private Long phoneNumber;
 
 }
