3b9262ae3f113a2b86726047c2cb2a2bd6d4d7da
MaxWell <xaxaxa25420@gmail.com>
2022-04-03 05:05:03 +0300

    fix

--- platform_orchestrator/src/main/java/com/override/repositories/PersonalDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.override.repositories;
-
-import com.override.models.JoinRequest;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonalDataRepository extends CrudRepository<JoinRequest, Long> {
-}
