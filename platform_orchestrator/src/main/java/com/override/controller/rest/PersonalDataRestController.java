5c35540c92e9c8380b7cc6313a25db892636cd15
MaxWell <xaxaxa25420@gmail.com>
2022-04-06 08:42:36 +0300

    Complete working service save method

--- platform_orchestrator/src/main/java/com/override/controller/rest/PersonalDataRestController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/PersonalDataRestController.java
@@ -5,7 +5,6 @@ import com.override.models.PlatformUser;
 import com.override.service.CustomStudentDetailService;
 import com.override.service.PersonalDataService;
 import com.override.service.PlatformUserService;
-import dtos.PersonalDataDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +24,7 @@ public class PersonalDataRestController {
         return platformUserService.findPlatformUserByLogin(user.getUsername());
     }
 
-    @PatchMapping
+    @PatchMapping("{userLogin}")
     public void patch(@RequestBody PersonalData personalData,
                       @PathVariable String userLogin) {
         personalDataService.save(personalData, userLogin);
