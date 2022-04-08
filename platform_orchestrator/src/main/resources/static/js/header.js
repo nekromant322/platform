6b6ea1c3ea127091d47ee12c48e9c557424971e5
scampshnik <kitnek00@gmail.com>
2022-04-08 23:19:27 +0300

    change fragment and fragment name in .html files

--- platform_orchestrator/src/main/resources/static/js/header.js
+++ platform_orchestrator/src/main/resources/static/js/header.js
@@ -1,4 +1,19 @@
+window.onload = getNavbar();
+
 function logout() {
     deleteCookie("token");
     window.location.href = "/login";
+
+}
+
+function getNavbar() {
+    $.ajax({
+        url: '/navbar',
+        type: 'GET',
+        contentType: 'application/json',
+        async: false,
+        success: function (navbar) {
+            document.getElementById("navbar").innerHTML = navbar
+        }
+    });
 }
