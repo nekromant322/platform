314d0365c20c0486e73cf6f95b0e4cedea3e44aa
MaxWell <xaxaxa25420@gmail.com>
2022-04-09 13:07:47 +0300

    refactoring

--- platform_orchestrator/src/main/java/com/override/service/ActGenerationService.java
+++ platform_orchestrator/src/main/java/com/override/service/ActGenerationService.java
@@ -1,6 +1,8 @@
 package com.override.service;
 
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.BaseFont;
+import com.override.exception.InvalidPersonalDataException;
 import com.override.models.PersonalData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,7 @@ public class ActGenerationService {
 
         try {
             OutputStream outputStream = new FileOutputStream("act" + personalData.getActNumber() + ".pdf");
+
             String processHTML = templateEngine.process("docs/actGenerationTemplate",
                     contextCreation(personalData));
 
@@ -35,8 +38,11 @@ public class ActGenerationService {
             renderer.createPDF(outputStream, false);
             renderer.finishPDF();
             outputStream.close();
-        } catch (Exception e) {
+
+        } catch (DocumentException | IOException e) {
             log.warn("При создании акта №" + personalData.getActNumber() + " произошла ошибка!");
+        } catch (NullPointerException e) {
+            throw new InvalidPersonalDataException("Данные пользователя содержат пустые поля");
         }
     }
 
@@ -46,23 +52,20 @@ public class ActGenerationService {
 
         Context context = new Context();
 
-        try {
-            context.setVariable("actNumber", personalData.getActNumber());
-            context.setVariable("contractNumber", personalData.getContractNumber());
-            context.setVariable("date", formatter.format(personalData.getDate()));
-            context.setVariable("fullName", personalData.getFullName());
-            context.setVariable("passportSeries", personalData.getPassportSeries());
-            context.setVariable("passportNumber", personalData.getPassportNumber());
-            context.setVariable("passportIssued", personalData.getPassportIssued());
-            context.setVariable("issueDate", formatter.format(personalData.getIssueDate()));
-            context.setVariable("birthDate", formatter.format(personalData.getBirthDate()));
-            context.setVariable("registration", personalData.getRegistration());
-            context.setVariable("email", personalData.getEmail());
-            context.setVariable("phoneNumber", personalData.getPhoneNumber());
-            context.setVariable("actualDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        } catch (NullPointerException e) {
-            log.warn("Данные пользователя содержат пустые поля");
-        }
+        context.setVariable("actNumber", personalData.getActNumber());
+        context.setVariable("contractNumber", personalData.getContractNumber());
+        context.setVariable("date", formatter.format(personalData.getDate()));
+        context.setVariable("fullName", personalData.getFullName());
+        context.setVariable("passportSeries", personalData.getPassportSeries());
+        context.setVariable("passportNumber", personalData.getPassportNumber());
+        context.setVariable("passportIssued", personalData.getPassportIssued());
+        context.setVariable("issueDate", formatter.format(personalData.getIssueDate()));
+        context.setVariable("birthDate", formatter.format(personalData.getBirthDate()));
+        context.setVariable("registration", personalData.getRegistration());
+        context.setVariable("email", personalData.getEmail());
+        context.setVariable("phoneNumber", personalData.getPhoneNumber());
+        context.setVariable("actualDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+
         return context;
     }
 }
