ab95bccd5152086abf8889ab468a95d67a128900
scampshnik <kitnek00@gmail.com>
2022-04-09 20:44:21 +0300

    add cache for navbar

--- platform_orchestrator/src/main/resources/static/js/header.js
+++ platform_orchestrator/src/main/resources/static/js/header.js
@@ -23,3 +23,59 @@ function getNavbar() {
         }
     });
 }
+
+// Try to get data from the cache, but fall back to fetching it live.
+async function getData() {
+    const cacheVersion = 1;
+    const cacheName = `myapp-${cacheVersion}`;
+    const url = '/navbar';
+    let cachedData = await getCachedData(cacheName, url);
+
+    if (cachedData) {
+        console.log('Retrieved cached data');
+        return cachedData;
+    }
+
+    console.log('Fetching fresh data');
+
+    const cacheStorage = await caches.open(cacheName);
+    await cacheStorage.add(url);
+    cachedData = await getCachedData(cacheName, url);
+    await deleteOldCaches(cacheName);
+
+    return cachedData;
+}
+
+// Get data from the cache.
+async function getCachedData(cacheName, url) {
+    const cacheStorage = await caches.open(cacheName);
+    const cachedResponse = await cacheStorage.match(url);
+
+    if (!cachedResponse || !cachedResponse.ok) {
+        return false;
+    }
+
+    return await cachedResponse.json();
+}
+
+// Delete any old caches to respect user's disk space.
+async function deleteOldCaches(currentCache) {
+    const keys = await caches.keys();
+
+    for (const key of keys) {
+        const isOurCache = 'myapp-' === key.substr(0, 6);
+
+        if (currentCache === key || !isOurCache) {
+            continue;
+        }
+
+        caches.delete(key);
+    }
+}
+
+try {
+    const data = getData();
+    console.log({data});
+} catch (error) {
+    console.error({error});
+}
