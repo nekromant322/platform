e0bc4239146348422caa9c9b92626dd984ba2162
MaxWell <xaxaxa25420@gmail.com>
2022-04-09 05:02:39 +0300

    Refactoring + NPE catch

--- platform_orchestrator/src/main/java/com/override/service/ActGenerationService.java
+++ platform_orchestrator/src/main/java/com/override/service/ActGenerationService.java
@@ -14,8 +14,6 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @Slf4j
@@ -26,37 +24,37 @@ public class ActGenerationService {
 
     public void createPDF(PersonalData personalData) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        Map<String,Object> variables = new HashMap<>();
-        variables.put("actNumber", personalData.getActNumber());
-        variables.put("contractNumber", personalData.getContractNumber());
-        variables.put("date", formatter.format(personalData.getDate()));
-        variables.put("fullName", personalData.getFullName());
-        variables.put("passportSeries", personalData.getPassportSeries());
-        variables.put("passportNumber", personalData.getPassportNumber());
-        variables.put("passportIssued", personalData.getPassportIssued());
-        variables.put("issueDate", formatter.format(personalData.getIssueDate()));
-        variables.put("birthDate", formatter.format(personalData.getBirthDate()));
-        variables.put("registration", personalData.getRegistration());
-        variables.put("email", personalData.getEmail());
-        variables.put("phoneNumber", personalData.getPhoneNumber());
-        variables.put("actualDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-
         Context context = new Context();
-        context.setVariables(variables);
 
         try {
+            context.setVariable("actNumber", personalData.getActNumber());
+            context.setVariable("contractNumber", personalData.getContractNumber());
+            context.setVariable("date", formatter.format(personalData.getDate()));
+            context.setVariable("fullName", personalData.getFullName());
+            context.setVariable("passportSeries", personalData.getPassportSeries());
+            context.setVariable("passportNumber", personalData.getPassportNumber());
+            context.setVariable("passportIssued", personalData.getPassportIssued());
+            context.setVariable("issueDate", formatter.format(personalData.getIssueDate()));
+            context.setVariable("birthDate", formatter.format(personalData.getBirthDate()));
+            context.setVariable("registration", personalData.getRegistration());
+            context.setVariable("email", personalData.getEmail());
+            context.setVariable("phoneNumber", personalData.getPhoneNumber());
+            context.setVariable("actualDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+
             OutputStream outputStream = new FileOutputStream("act" + personalData.getActNumber() + ".pdf");
-            String processHTML = templateEngine.process("actGenerationTemplate", context);
+            String processHTML = templateEngine.process("docs/actGenerationTemplate", context);
 
             ITextRenderer renderer = new ITextRenderer();
             renderer.setDocumentFromString(processHTML);
             renderer.getFontResolver().addFont("/fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-            renderer.createPDF(outputStream, false);
             renderer.layout();
+            renderer.createPDF(outputStream, false);
             renderer.finishPDF();
             outputStream.close();
         } catch (DocumentException | IOException e) {
             log.warn("При создании акта №" + personalData.getActNumber() + " произошла ошибка!");
+        } catch (NullPointerException e) {
+            log.warn("Данные пользователя содержат пустые поля");
         }
     }
 }
