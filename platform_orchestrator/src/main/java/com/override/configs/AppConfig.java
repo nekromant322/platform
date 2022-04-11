c2ee7510de3426266856910d3a5b1cdae407c239
MaxWell <xaxaxa25420@gmail.com>
2022-04-11 05:27:29 +0300

    ITextRenderer -> bean

--- platform_orchestrator/src/main/java/com/override/configs/AppConfig.java
+++ platform_orchestrator/src/main/java/com/override/configs/AppConfig.java
@@ -1,14 +1,32 @@
 package com.override.configs;
 
 import com.github.javafaker.Faker;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.BaseFont;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.IOException;
 
 @Configuration
+@Slf4j
 public class AppConfig {
 
     @Bean
     public Faker faker() {
         return new Faker();
+    }
+
+    @Bean
+    public ITextRenderer iTextRenderer() {
+        ITextRenderer iTextRenderer = new ITextRenderer();
+        try {
+            iTextRenderer.getFontResolver().addFont("/fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        } catch (DocumentException | IOException e) {
+            log.warn("Ошибка при настройке шрифта ITextRenderer");
+        }
+        return iTextRenderer;
     }
 }
