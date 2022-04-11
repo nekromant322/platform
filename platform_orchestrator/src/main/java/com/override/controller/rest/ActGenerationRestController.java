39f052a0dca35c946fe4755e930b38f5d2dcacf7
MaxWell <xaxaxa25420@gmail.com>
2022-04-11 05:28:09 +0300

    RestController mapping edit

--- platform_orchestrator/src/main/java/com/override/controller/rest/ActGenerationRestController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/ActGenerationRestController.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/actGeneration")
+@RequestMapping("/act")
 public class ActGenerationRestController {
 
     @Autowired
