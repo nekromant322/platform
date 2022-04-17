acee69bef659df8cd6e27a5a2fad08d355ad3dfc
MaxWell <xaxaxa25420@gmail.com>
2022-04-17 20:59:51 +0300

    fix code + realized new feature

--- platform_orchestrator/src/main/resources/static/js/lessonSidebar.js
+++ platform_orchestrator/src/main/resources/static/js/lessonSidebar.js
@@ -1,3 +1,5 @@
+let allLessonList = [];
+
 window.onload = init(findCourseName());
 
 function findCourseName() {
@@ -13,7 +15,7 @@ function init(courseName) {
         url: "/lessons/structureOf/" + courseName,
         async: false,
         dataType: "json",
-        success: function(data) {
+        success: function (data) {
             structure = data;
         },
         error: function (error) {
@@ -21,26 +23,35 @@ function init(courseName) {
             console.log(error);
         }
     });
-    document.getElementById("sideBarBody").innerHTML = " ";
-    document.getElementById("sideBarBody").innerHTML = gatherText(structure, courseName);
+
+    window.document.getElementById("courseNameDiv").innerText = courseName;
+    window.document.getElementById("sideBarBody").innerHTML = gatherText(structure, courseName);
+    checkPassedLessons(structure, courseName)
+
 }
 
 function gatherText(structureOfCourse, courseName) {
     let text = "";
     let nameOfLesson = "";
+    let fullNameOfCourse = "";
     for (let chapterKey in structureOfCourse) {
         text += ' <li className="active"> ';
-        text += ` <a href="#${chapterKey}Submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${chapterKey}</a> \n`;
+        text += ` <a href="#${chapterKey}Submenu" data-toggle="collapse" aria-expanded="false" 
+                  id="chapterKey-${courseName}-${chapterKey}" class="dropdown-toggle">${chapterKey}</a> \n`;
         text += ` <ul class="collapse list-unstyled" id="${chapterKey}Submenu"> \n`;
         for (let stepKey in structureOfCourse[chapterKey]) {
-            text += `<li> <a href="#${chapterKey}-${stepKey}Submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${stepKey}</a> </li>`;
+            text += `<li> <a href="#${chapterKey}-${stepKey}Submenu" data-toggle="collapse" aria-expanded="false" 
+                      id="stepKey-${courseName}-${chapterKey}-${stepKey}" class="dropdown-toggle" >${stepKey}</a> </li>`;
             text += ` <ul class="collapse list-unstyled" id="${chapterKey}-${stepKey}Submenu"> \n`;
-            for (let lessonKey in stepKey) {
+            for (let lessonKey in (structureOfCourse[chapterKey])[stepKey]) {
                 text += '<li>';
                 if (((structureOfCourse[chapterKey])[stepKey])[lessonKey] !== undefined) {
                     nameOfLesson = String(((structureOfCourse[chapterKey])[stepKey])[lessonKey]);
                     nameOfLesson = nameOfLesson.substr(0, nameOfLesson.length - 5);
-                    text += `<a href="/lessons/${courseName}/${chapterKey}/${stepKey}/${nameOfLesson}">${nameOfLesson}</a> \n`; /*TODO почему-то /core/*/
+                    fullNameOfCourse = courseName + "-" + chapterKey + "-" + stepKey + "-" + nameOfLesson;
+                    text += `<a href="/lessons/${courseName}/${chapterKey}/${stepKey}/${nameOfLesson}"
+                                id="lesson-${courseName}-${chapterKey}-${stepKey}-${nameOfLesson}">${nameOfLesson}</a> \n`;
+                    allLessonList.push(fullNameOfCourse);
                 }
                 text += '</li>';
             }
@@ -50,4 +61,64 @@ function gatherText(structureOfCourse, courseName) {
         text += ' </li> ';
     }
     return text;
+}
+
+function checkPassedLessons(structure, courseName) {
+    $.ajax({
+        method: "GET",
+        url: "/lessonProgress/allStat",
+        dataType: "json",
+        cache: false,
+        success: function (passedLessons) {
+            $.each(passedLessons, function (i, passedLesson) {
+                $.each(allLessonList, function (i, lesson) {
+                    if (lesson === passedLesson) {
+                        document.getElementById("lesson-" + passedLesson).style.color = "yellowgreen";
+                    }
+                })
+            })
+            let lessonCount;
+            let passedLessonCount;
+            let stepCount;
+            let passedStepCount;
+            let nameOfLesson;
+            for (let chapterKey in structure) {
+                stepCount = 0;
+                passedStepCount = 0;
+
+                for (let stepKey in structure[chapterKey]) {
+                    lessonCount = 0;
+                    passedLessonCount = 0;
+                    stepCount++;
+
+                    for (let lessonKey in (structure[chapterKey])[stepKey]) {
+                        nameOfLesson = String(((structure[chapterKey])[stepKey])[lessonKey]);
+                        nameOfLesson = nameOfLesson.substr(0, nameOfLesson.length - 5);
+                        lessonCount++;
+
+                        if (document.getElementById("lesson-" + courseName + "-" +
+                            chapterKey + "-" + stepKey + "-" + nameOfLesson).style.color === "yellowgreen") {
+
+                            passedLessonCount++;
+                        }
+                    }
+
+                    if (passedLessonCount === lessonCount) {
+                        document.getElementById("stepKey-" + courseName + "-" +
+                            chapterKey + "-" + stepKey).style.color = "yellowgreen"
+                        passedStepCount++
+                    }
+                }
+
+                if (passedStepCount === stepCount) {
+                    document.getElementById("chapterKey-" + courseName + "-" +
+                        chapterKey).style.color = "yellowgreen"
+                }
+            }
+        },
+        error: function (error) {
+            console.log("Error!");
+            console.log(error);
+        }
+    });
 }
