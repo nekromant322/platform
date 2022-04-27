a348289f128e074cdd2d4260d3eda40667cbfdba
MaxWell <xaxaxa25420@gmail.com>
2022-04-28 02:29:54 +0300

    refactoring

--- platform_orchestrator/src/main/java/com/override/repositories/DocumentRepository.java
+++ platform_orchestrator/src/main/java/com/override/repositories/DocumentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
-    List<Document> findAllByUserId (Long id);
+    List<Document> findAllByUserId(Long id);
 }
