20c82d1f2e7915bc829b37af6652dc6fe22b79f9
MaxWell <xaxaxa25420@gmail.com>
2022-04-27 07:18:59 +0300

    fix

--- platform_orchestrator/src/main/java/com/override/repositories/PersonalDataRepository.java
+++ platform_orchestrator/src/main/java/com/override/repositories/PersonalDataRepository.java
@@ -1,9 +1,7 @@
 package com.override.repositories;
 
-import com.override.models.JoinRequest;
 import com.override.models.PersonalData;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonalDataRepository extends CrudRepository<JoinRequest, Long> {
-    PersonalData save(PersonalData data);
+public interface PersonalDataRepository extends CrudRepository<PersonalData, Long> {
 }
