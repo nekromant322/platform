695a0d9bf29fb2908851a67d9c961ce7f446d78f
MaxWell <xaxaxa25420@gmail.com>
2022-04-28 02:40:42 +0300

    refactoring

--- platform_orchestrator/src/main/java/com/override/repositories/LessonProgressRepository.java
+++ platform_orchestrator/src/main/java/com/override/repositories/LessonProgressRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LessonProgressRepository extends JpaRepository<LessonProgress, Long> {
-    List<LessonProgress> findAllByUserId (Long id);
+    List<LessonProgress> findAllByUserId(Long id);
 }
