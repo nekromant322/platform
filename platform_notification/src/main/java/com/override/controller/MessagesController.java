5514b889ea30e8422c53586cd6ac2ed46cb83181
yusov8400 <yusov01@mail.ru>
2022-05-23 23:10:01 +0300

    add endpoint

--- platform_notification/src/main/java/com/override/controller/MessagesController.java
+++ platform_notification/src/main/java/com/override/controller/MessagesController.java
@@ -3,13 +3,9 @@ package com.override.controller;
 import com.override.service.MessageService;
 import enums.Communication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/communications")
@@ -31,5 +27,10 @@ public class MessagesController {
             @RequestParam("message") String message,
             @RequestParam("type") Communication... type) {
         messageService.sendMessage(login, message, type);
+    }
+
+    @GetMapping("/check")
+    public List<Communication> CheckNotificationMethods(@RequestParam("login")String login){
+        return messageService.checkNotificationMethods(login);
     }
 }
