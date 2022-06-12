43d95944ffc934eee310189b6ffe72679b4546d8
yusov8400 <yusov01@mail.ru>
2022-06-12 18:46:22 +0300

    change profile and redirect

--- platform_orchestrator/src/main/java/com/override/controller/InitializationController.java
+++ platform_orchestrator/src/main/java/com/override/controller/InitializationController.java
@@ -2,20 +2,22 @@ package com.override.controller;
 
 import com.override.util.InitializationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Profile("dev")
 @RequestMapping("init")
 public class InitializationController {
 
     @Autowired
-    InitializationService initializationService;
+    private InitializationService initializationService;
 
     @GetMapping
     public String init(){
         initializationService.initTestData();
-        return "redirect:http://localhost:8000/";
+        return "redirect:";
     }
 }
