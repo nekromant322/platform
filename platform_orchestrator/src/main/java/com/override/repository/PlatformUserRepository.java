6536aa08526ffc880fce688b6946875bfdbfe947
Sofia <sskelep@gmail.com>
2022-07-18 02:58:10 +0300

    работающий фикс)

--- platform_orchestrator/src/main/java/com/override/repository/PlatformUserRepository.java
+++ platform_orchestrator/src/main/java/com/override/repository/PlatformUserRepository.java
@@ -25,23 +25,16 @@ public interface PlatformUserRepository extends CrudRepository<PlatformUser, Lon
             nativeQuery = true)
     List<PlatformUser> findStudentsWithoutReportOfCurrentDay();
 
-    @Query(value = "select p_user.id, p_user.login, p_user.password " +
-            "from platform_user p_user " +
-            "where p_user.id in (select authorities.user_id " +
-            "                    from users_authorities authorities " +
-            "                             join role_t role on role.id = authorities.authority_id " +
-            "                    where role.authority like :roleName) " +
-            "group by p_user.id, p_user.login, p_user.password",
-            nativeQuery = true)
+    @Query("select p_user " +
+            "from PlatformUser p_user " +
+            "where exists ( " +
+            "select authority from p_user.authorities authority where authority.authority = :roleName)")
     List<PlatformUser> findAllByAuthorityName(@Param("roleName") String roleName);
 
-    @Query(value = "select p_user.id, p_user.login, p_user.password " +
-            "from platform_user p_user " +
-            "where p_user.id not in (select authorities.user_id " +
-            "                    from users_authorities authorities " +
-            "                             join role_t role on role.id = authorities.authority_id " +
-            "                    where role.authority like :roleName) " +
-            "group by p_user.id, p_user.login, p_user.password",
-            nativeQuery = true)
+
+    @Query("select p_user " +
+            "from PlatformUser p_user " +
+            "where not exists ( " +
+            "select authority from p_user.authorities authority where authority.authority = :roleName)")
     List<PlatformUser> findByAuthoritiesNamesNotContaining(@Param("roleName") String roleName);
 }
