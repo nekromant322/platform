85c1f88f703c8fa4b7141adefd5c29ec5247e661
arturchik804 <evlantevartur@gmail.com>
2022-08-03 19:37:48 +0300

    feature/BQFI-19/integrationVKcalls

--- platform_orchestrator/src/main/java/com/override/service/ReviewService.java
+++ platform_orchestrator/src/main/java/com/override/service/ReviewService.java
@@ -36,11 +36,11 @@ public class ReviewService {
     @Autowired
     private NotificatorFeign notificatorFeign;
 
-    public static String CONFIRMED_REVIEW_MESSAGE_TELEGRAM = "Ментор %s подтвердил ревью %s в %s";
+    public static String CONFIRMED_REVIEW_MESSAGE_TELEGRAM = "Ментор %s подтвердил ревью %s в %s, ссылка на звонок: %s";
     public static String DELETED_REVIEW_MESSAGE_TELEGRAM = "Ментор вынужден был отменить ревью. " +
             "Попробуйте записаться на другое время";
-    public static String CHANGED_REVIEW_TIME_MESSAGE_TELEGRAM = "Ментор изменил время ревью. Ревью пройдет %s в %s";
-    public static String CHANGED_REVIEW_MENTOR_MESSAGE_TELEGRAM = "На Ваше ревью сменился ментор. Его проведет: %s %s в %s ";
+    public static String CHANGED_REVIEW_TIME_MESSAGE_TELEGRAM = "Ментор изменил время ревью. Ревью пройдет %s в %s, ссылка на звонок: %s";
+    public static String CHANGED_REVIEW_MENTOR_MESSAGE_TELEGRAM = "На Ваше ревью сменился ментор. Его проведет: %s %s в %s, ссылка на звонок: %s";
     public static String NEW_REVIEW_MESSAGE_TELEGRAM = "Новый запрос на ревью от пользователя %s";
 
     /**
@@ -74,16 +74,19 @@ public class ReviewService {
                 reviewDTO.setStudentLogin(userLogin);
             }
         } else if (reviewDTO.getBookedTime() != null && reviewDTO.getMentorLogin() == "") {
+            reviewDTO.setCallLink(startCall());
             sendMessage(reviewDTO.getStudentLogin(), String.format(CONFIRMED_REVIEW_MESSAGE_TELEGRAM, userLogin,
-                    reviewDTO.getBookedDate(), reviewDTO.getBookedTime()), Communication.TELEGRAM);
+                    reviewDTO.getBookedDate(), reviewDTO.getBookedTime(), reviewDTO.getCallLink()), Communication.TELEGRAM);
             reviewDTO.setMentorLogin(userLogin);
         } else if (reviewDTO.getMentorLogin() != null && reviewDTO.getBookedTime() != null) {
             if (!reviewDTO.getMentorLogin().equals(userLogin)) {
+                reviewDTO.setCallLink(startCall());
                 sendMessage(reviewDTO.getStudentLogin(), String.format(CHANGED_REVIEW_MENTOR_MESSAGE_TELEGRAM,
-                        userLogin, reviewDTO.getBookedTime(), reviewDTO.getBookedDate()), Communication.TELEGRAM);
+                        userLogin, reviewDTO.getBookedDate(), reviewDTO.getBookedTime(), reviewDTO.getCallLink()), Communication.TELEGRAM);
             } else {
+                reviewDTO.setCallLink(startCall());
                 sendMessage(reviewDTO.getStudentLogin(), String.format(CHANGED_REVIEW_TIME_MESSAGE_TELEGRAM,
-                        reviewDTO.getBookedTime(), reviewDTO.getBookedDate()), Communication.TELEGRAM);
+                        reviewDTO.getBookedDate(), reviewDTO.getBookedTime(), reviewDTO.getCallLink()), Communication.TELEGRAM);
             }
         }
         reviewRepository.save(reviewMapper.dtoToEntity(reviewDTO,
@@ -146,12 +149,15 @@ public class ReviewService {
                     String messageText = "Скоро ревью у @" + review.getStudent().getLogin() +
                             " с @" + review.getMentor().getLogin() + "\n" +
                             review.getBookedDate() + " " + review.getBookedTime() +
-                            "\nТема: " + review.getTopic();
+                            "\nТема: " + review.getTopic() + "\n" +
+                            "Ссылка на звонок: " + review.getCallLink();
                     notificatorFeign.sendMessage(review.getStudent().getLogin(), messageText, Communication.TELEGRAM);
                     notificatorFeign.sendMessage(review.getMentor().getLogin(), messageText, Communication.TELEGRAM);
                 });
     }
 
-
+    public String startCall(){
+        return  "https://vk.com/call/join/4WaALCm7XxBMugVAKkafGQ8O6RDi8pDpqHNJs8eW5eyQydvAF49IuShz0oqK5uzdAVuT1dBQcbzZ6ThMUUCjT7dSRQ15oV2ycQTUgZPk3Io";
+    }
 }
 
