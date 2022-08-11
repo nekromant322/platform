9eee4650d5f4578b1d904b24590f1abe1f9a7c15
Ivan <vanysemka@gmail.com>
2022-08-11 19:54:39 +0300

    Moved controller to REST

--- platform_orchestrator/src/main/java/com/override/controller/rest/OfferDocumentRestController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/OfferDocumentRestController.java
@@ -1,4 +1,4 @@
-package com.override.controller;
+package com.override.controller.rest;
 
 import com.override.model.OfferDocument;
 import com.override.service.OfferDocumentService;
@@ -9,31 +9,30 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/offer-document")
-public class OfferDocumentController {
+public class OfferDocumentRestController {
 
     @Autowired
     private OfferDocumentService offerDocumentService;
 
     @PostMapping("/upload/{reportId}")
-    public String uploadFile(@PathVariable("reportId") Long reportId,
-                             @RequestParam("file") MultipartFile multipartFile) throws IOException {
+    public void uploadFile(@PathVariable("reportId") Long reportId,
+                           @RequestParam("file") MultipartFile multipartFile,
+                           HttpServletResponse response) throws IOException {
 
         offerDocumentService.upload(multipartFile, reportId);
+        response.sendRedirect("/interviewReports");
 
-        return "redirect:/interviewReports";
     }
 
     @GetMapping("/download/{id}")
-    @ResponseBody
     @Transactional
     public ResponseEntity<Resource> downloadFile(@PathVariable("id") Long id) {
 
@@ -47,7 +46,6 @@ public class OfferDocumentController {
     }
 
     @GetMapping("/{reportId}")
-    @ResponseBody
     public OfferDocumentDTO getFileDTO(@PathVariable("reportId") Long reportId) {
         return offerDocumentService.getByReportId(reportId);
     }
