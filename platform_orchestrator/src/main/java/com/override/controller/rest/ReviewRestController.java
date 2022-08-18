a6dcc0d4ae1b3319793ffdcece24259739ede619
arturchik804 <evlantevartur@gmail.com>
2022-08-18 03:30:20 +0300

    Добавил чутка описания, подправил косяки и конфликты

--- platform_orchestrator/src/main/java/com/override/controller/rest/ReviewRestController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/ReviewRestController.java
@@ -25,8 +25,7 @@ public class ReviewRestController {
     private ReviewService reviewService;
 
     @PatchMapping
-    @ApiOperation(value = "Сохраняет новое или изменяет существующее ревью.\n" +
-            "Варианты условий:\n" +
+    @ApiOperation(notes = "Варианты условий:\n" +
             "reviewDTO.getId() == null, reviewDTO.getStudentLogin() == null — будет отправлено уведомление в Telegram\n" +
             "ментору о новом запросе на ревью от студента. Имя пользователя студента отправляется в reviewDTO.\n" +
             "Когда ревью только создается, подразумевается, что этот запрос делает студент,\n" +
@@ -38,23 +37,22 @@ public class ReviewRestController {
             "reviewDTO.getMentorLogin() != null && reviewDTO.getBookedTime() != null - изменения вносятся в уже\n" +
             "подтвержденное ревью. А дальше уже два варианта:\n" +
             "!reviewDTO.getMentorLogin().equals(userLogin) - будет отправлено уведомление о смене ментора с указанием времени.\n" +
-            "reviewDTO.getMentorLogin().equals(userLogin) - если логины совпадают, отправляется уведомление об изменении времени.\n" +
-            "@param reviewDTO проверяет информацию, полученную из запроса\n" +
-            "@param userLogin имя пользователя, делающего запрос")
+            "reviewDTO.getMentorLogin().equals(userLogin) - если логины совпадают, отправляется уведомление об изменении времени",
+            value = "Сохраняет новое или изменяет существующее ревью.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Ревью сохранено!")
     })
     public ResponseEntity<String> saveOrUpdateReview(@RequestBody @ApiParam(value = "reviewDTO проверяет информацию, полученную из запроса") ReviewDTO reviewDTO,
-                                                     @AuthenticationPrincipal @ApiParam(value = "Отсюда достанем имя пользователя, делающего запрос") CustomStudentDetailService.CustomStudentDetails user) {
+                                                     @AuthenticationPrincipal CustomStudentDetailService.CustomStudentDetails user) {
         reviewService.saveOrUpdate(reviewDTO, user.getUsername());
         return new ResponseEntity<>("Ревью сохранено!", HttpStatus.OK);
     }
 
     @PostMapping
-    @ApiOperation(value = "Поиск выбранных ревью с использованием фильтра.\n" +
+    @ApiOperation(notes = "Поиск выбранных ревью с использованием фильтра.\n" +
             "Ревью можно найти по логину студента или ментора, а также по дате.\n" +
             "Если эти параметры имеют значение null, то отбираются новые ревью, которым еще не назначены ментор и время",
-            notes = "Возвращает список ReviewDTO, полученный путем сопоставления списка найденных отзывов.")
+            value = "Возвращает список ревью, полученный путем сопоставления списка найденных отзывов.")
     public List<ReviewDTO> findReview(@RequestBody @ApiParam(value = "reviewFilterDTO поступает из запроса") ReviewFilterDTO reviewFilterDTO) {
         return reviewService.find(reviewFilterDTO);
     }
