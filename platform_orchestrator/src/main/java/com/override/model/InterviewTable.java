516c5ab036ffcb63537789c5dc327b4c3f984124
Silchukelisa <Silchuk.liza14@gmail.com>
2022-08-22 22:26:04 +0300

    Табличка для удобного собеседования для каждого студента

--- platform_orchestrator/src/main/java/com/override/model/InterviewTable.java
+++ platform_orchestrator/src/main/java/com/override/model/InterviewTable.java
@@ -15,7 +15,7 @@ import java.time.LocalTime;
 @Entity
 public class InterviewTable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user_login")
