ecfe74d04b66bc5deda73c33d1b83298467c9394
arturchik804 <evlantevartur@gmail.com>
2022-09-02 21:38:48 +0300

    Фильтр для отчета студента

--- platform_orchestrator/src/main/java/com/override/config/PermitAllURLs.java
+++ platform_orchestrator/src/main/java/com/override/config/PermitAllURLs.java
@@ -6,7 +6,13 @@ import org.springframework.stereotype.Component;
 public class PermitAllURLs {
     static String[] urls = (new String[]{"/login", "/init", "/css/*", "/images/*", "/js/*"});
 
+    static String[] urlsForReportFilter = (new String[]{"/login", "/init", "/css/*", "/images/*", "/js/*", "css", "js","bugs", "navbar"});
+
     public String[] getPermitAllUrls() {
         return urls;
+    }
+
+    public String[] getPermitAllUrlsForReportFilter() {
+        return urlsForReportFilter;
     }
 }
