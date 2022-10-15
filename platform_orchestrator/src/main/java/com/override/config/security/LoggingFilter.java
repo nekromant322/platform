25e2b433e3ae5f30ba1dc2ea40e01a0d8cc8096b
Silchukelisa <Silchuk.liza14@gmail.com>
2022-10-16 02:11:14 +0300

    Убрала логи для .js, .css, .images

---
+++ platform_orchestrator/src/main/java/com/override/config/security/LoggingFilter.java
@@ -0,0 +1,49 @@
+package com.override.config.security;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+
+        filterChain.doFilter(requestWrapper, responseWrapper);
+
+
+        Map<String, String> map = new HashMap<>();
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        if (!request.getRequestURI().contains("js") && !request.getRequestURI().contains("css") && !request.getRequestURI().contains("images")) {
+            LOGGER.info("\n METHOD={};\n REQUESTURI={};\n RESPONSE CODE={};\n HEADER={};\n ",
+                    request.getMethod(), request.getRequestURI(), response.getStatus(), map);
+        }
+        responseWrapper.copyBodyToResponse();
+    }
+}
