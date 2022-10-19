c6ce8d416363b3bf9e0e7d19a700008b4ed40a87
Silchukelisa <Silchuk.liza14@gmail.com>
2022-10-19 16:03:20 +0300

    Стартер

--- override-logging-starter/src/main/java/com/override/config/LoggingConfig.java
+++ override-logging-starter/src/main/java/com/override/config/LoggingConfig.java
@@ -1,11 +1,12 @@
-package com.override;
+package com.override.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.override.LoggingFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
+@ConditionalOnClass(LoggingConfig.class)
 public class LoggingConfig {
 
     @Bean
