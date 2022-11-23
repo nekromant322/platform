f7e0fd0499116d6a5edd956686969a906a57ad01
Tolmachevsky <Tolmachevsky001@ya.ru>
2022-11-23 17:27:59 +0300

    Code has been improved

--- platform_orchestrator/src/main/java/com/override/feign/StepikCodeExecutorFeign.java
+++ platform_orchestrator/src/main/java/com/override/feign/StepikCodeExecutorFeign.java
@@ -1,6 +1,7 @@
 package com.override.feign;
 
 import dto.StepikTokenDTO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.*;
 public interface StepikCodeExecutorFeign {
 
     @PostMapping(value = "/api/submissions", consumes = "application/json", produces = "application/json")
+    @ApiOperation(value = "Отправляет код на проверку StepikAPI")
     void execute(@RequestHeader(value = "Authorization", required = true) String token, @RequestBody String json);
 
     @GetMapping(value = "/api/submissions?limit=1&order=desc", consumes = "application/json", produces = "application/json")
+    @ApiOperation(value = "Зарашивает результаты проверки на конкретную задачу - numberOfStep")
     String getResult(@RequestParam(value = "step") String numberOfStep, @RequestHeader(value = "Authorization", required = true) String token);
 
     @PostMapping(value = "/oauth2/token/?grant_type=client_credentials", consumes = "application/json", produces = "application/json")
+    @ApiOperation(value = "Запрашивает токен у StepikAPI по логину и паролю клиента")
     StepikTokenDTO getToken(@RequestHeader(value = "Authorization", required = true) String auth);
 }
--- platform_orchestrator/src/main/java/com/override/service/StepikService.java
+++ platform_orchestrator/src/main/java/com/override/service/StepikService.java
@@ -2,6 +2,7 @@ package com.override.service;
 
 import com.override.feign.StepikCodeExecutorFeign;
 import dto.StepikTokenDTO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.Cache;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StepikService {
 
-    @Value("${orchestrator.authorization}")
+    @Value("${stepik.authorization}")
     private String AUTHORIZATION;
 
     @Autowired
@@ -20,12 +21,14 @@ public class StepikService {
     @Autowired
     private StepikCodeExecutorFeign stepikCodeExecutorFeign;
 
+    @ApiOperation(value = "Кладет токен в Кэш")
     public void putStepikTokenInCache(StepikTokenDTO stepikTokenDTO) {
         Cache data = cacheManager.getCache("stepikToken");
         String token = stepikTokenDTO.getAccess_token();
         data.put("token", "Bearer " + token);
     }
 
+    @ApiOperation(value = "Запрашивает токен у StepikAPI по логину и паролю клиента - AUTHORIZATION")
     public String getStepikToken() {
         Cache data = cacheManager.getCache("stepikToken");
         if (data.get("token") == null) {
