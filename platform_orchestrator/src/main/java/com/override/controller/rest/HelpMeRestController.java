9509d1858891ac96ed67be17c84b27872a83f662
Your Name <you@example.com>
2022-12-20 23:52:21 +0300

    BQFI-89 Codestyle in HelpMeRestController, inject through field

--- platform_orchestrator/src/main/java/com/override/controller/rest/HelpMeRestController.java
+++ platform_orchestrator/src/main/java/com/override/controller/rest/HelpMeRestController.java
@@ -15,22 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelpMeRestController {
 
-    private final Cache<Integer, HelpMeDTO> helpMeDTOCache;
-
     @Autowired
-    public HelpMeRestController(Cache<Integer, HelpMeDTO> helpMeDTOCache) {
-        this.helpMeDTOCache = helpMeDTOCache;
-    }
+    private Cache<Integer, HelpMeDTO> helpMeDTOCache;
 
     @PostMapping("/helpMe")
-    int postHelpMeDTO(@RequestBody HelpMeDTO helpMeDTO) {
+    public int postHelpMeDTO(@RequestBody HelpMeDTO helpMeDTO) {
         helpMeDTOCache.put(helpMeDTO.hashCode(), helpMeDTO);
         log.info("Added to cache, key value {}", helpMeDTO.hashCode());
         return helpMeDTO.hashCode();
     }
 
     @GetMapping("/helpMe/cache/{key}")
-    HelpMeDTO getHelpMeCachedDTO(@PathVariable int key) {
+    public HelpMeDTO getHelpMeCachedDTO(@PathVariable int key) {
         log.info("Sended key {}", key);
         return helpMeDTOCache.getIfPresent(key); // здесь может выскочить null, обработку null завещаю фронту. А может и все ок
     }
